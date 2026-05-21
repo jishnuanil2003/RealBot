49cb19ea90ed57da307e2054e83345f238f580c3
Jishnu Anil <aniljishnu07@gmail.com>
2026-05-22 00:23:22 +0530

    Update prediction.py

--- Backend/prediction.py
+++ Backend/prediction.py
@@ -6,18 +6,19 @@ from langchain_community.document_loaders import UnstructuredURLLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from datetime import datetime
+import os
 
 load_dotenv()
 
 CHUNK_SIZE = 1000
-EMBEDDING_MODEL = "sentence-transformers/paraphrase-MiniLM-L3-v2"
+EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 LLM_MODEL = "llama-3.3-70b-versatile"
 VECTORSTORE_DIR = Path(__file__).parent / "resources/vectorstore"
 COLLECTION_NAME = "real_estate"
 
-llm =None
+llm = None
 vectordb = None
 
 def initialize():
@@ -25,15 +26,11 @@ def initialize():
 
     if llm is None:
         llm = ChatGroq(model=LLM_MODEL, temperature=0.9, max_tokens=500)
-    
+
     if vectordb is None:
-        hf = HuggingFaceEmbeddings(
-            model_name=EMBEDDING_MODEL,
-            model_kwargs={
-                "trust_remote_code": True,
-                "device": "cpu"
-            },
-            encode_kwargs={"normalize_embeddings": False}  # saves memory
+        hf = HuggingFaceEndpointEmbeddings(
+            model=EMBEDDING_MODEL,
+            huggingfacehub_api_token=os.getenv("HF_TOKEN")
         )
         vectordb = Chroma(
             collection_name=COLLECTION_NAME,
@@ -42,7 +39,6 @@ def initialize():
         )
 
 def process_urls(urls):
-
     yield "Initialize variables"
     initialize()
 
@@ -68,25 +64,16 @@ def process_urls(urls):
 
 def generate_answers(query):
     today = datetime.today().strftime("%Y-%m-%d")
-    prompt=f"""
+    prompt = f"""
     Today's date is {today}.
     {query}
     """
     if not vectordb:
         raise Exception("vectordb not initialized")
-    chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectordb.as_retriever())
+    chain = RetrievalQAWithSourcesChain.from_llm(
+        llm=llm,
+        retriever=vectordb.as_retriever()
+    )
     result = chain.invoke({"question": prompt}, return_only_outputs=True)
     sources = result.get("sources", "")
-    return result["answer"],sources
-
-
-# if __name__ == "__main__":
-#     urls = [
-#          "https://www.espncricinfo.com/team/india-6?utm_source=chatgpt.com",
-#         "https://www.bcci.tv/international/men/players/virat-kohli/2"
-#     ]
-#
-#     for status in process_urls(urls):
-#         print(status)
-#     answer = generate_answers("Tell me what is the age of VIRAT KOHLI ?")
-#     print(f"Answer: {answer}")
+    return result["answer"], sources
